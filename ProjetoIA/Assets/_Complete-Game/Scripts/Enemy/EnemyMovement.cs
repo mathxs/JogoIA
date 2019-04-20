aeb7d810cc187c954b1a440fff51d262e6da2073
mathxs <38170401+mathxs@users.noreply.github.com>
2019-04-20 16:45:59 -0300

    IA com 2 status
    Patrol e ataque

--- ProjetoIA/Assets/_Complete-Game/Scripts/Enemy/EnemyMovement.cs
+++ ProjetoIA/Assets/_Complete-Game/Scripts/Enemy/EnemyMovement.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.AI;
 
 namespace CompleteProject
 {
@@ -8,26 +9,45 @@ namespace CompleteProject
         Transform player;               // Reference to the player's position.
         PlayerHealth playerHealth;      // Reference to the player's health.
         EnemyHealth enemyHealth;        // Reference to this enemy's health.
-        UnityEngine.AI.NavMeshAgent nav;               // Reference to the nav mesh agent.
+        private NavMeshAgent nav;        // Reference to the nav mesh agent.
 
+        protected GameObject[] pointList;
+        protected Vector3 destPos;
+        protected bool curState;
+        protected int status;
+        public Transform inimigos;
 
-        void Awake ()
+        void Awake()
         {
             // Set up the references.
-            player = GameObject.FindGameObjectWithTag ("Player").transform;
-            playerHealth = player.GetComponent <PlayerHealth> ();
-            enemyHealth = GetComponent <EnemyHealth> ();
-            nav = GetComponent <UnityEngine.AI.NavMeshAgent> ();
+            //nav = GetComponent<NavMeshAgent>();
+            player = GameObject.FindGameObjectWithTag("Player").transform;
+            pointList = GameObject.FindGameObjectsWithTag("PatrolPoint");
+            //inimigos = GameObject.FindGameObjectsWithTag("Shootable");
+
+            playerHealth = player.GetComponent<PlayerHealth>();
+            enemyHealth = GetComponent<EnemyHealth>();
+            nav = GetComponent<UnityEngine.AI.NavMeshAgent>();
+            status = 1;
+
         }
 
 
-        void Update ()
+        void Update()
         {
             // If the enemy and the player have health left...
-            if(enemyHealth.currentHealth > 0 && playerHealth.currentHealth > 0)
+            if (enemyHealth.currentHealth > 0 && playerHealth.currentHealth > 0)
             {
-                // ... set the destination of the nav mesh agent to the player.
-                nav.SetDestination (player.position);
+                switch (status)
+                {
+                    case 0:
+                        UpdatePatrolState();
+                        break;
+                    case 1:
+                        UpdateAttackState();
+                        break;
+
+                }
             }
             // Otherwise...
             else
@@ -36,5 +56,51 @@ namespace CompleteProject
                 nav.enabled = false;
             }
         }
+
+        void UpdatePatrolState()
+        {
+            /*Debug.Log("Patrol");
+            Debug.Log(player.position.x);
+            Debug.Log(player.position.y);
+            Debug.Log(inimigos.position.x);
+            Debug.Log(inimigos.position.y);*/
+            //Debug.Log((Mathf.Abs(player.position.x - inimigos.position.x)));
+            //Debug.Log((Mathf.Abs(player.position.y - inimigos.position.y)));
+
+            int rndIndex = UnityEngine.Random.Range(0, pointList.Length);
+            destPos = pointList[rndIndex].transform.position;
+            // ... set the destination of the nav mesh agent to the player.
+            if ((Mathf.Abs(player.position.x - inimigos.position.x) < 5.0) && (Mathf.Abs(player.position.y - inimigos.position.y) < 5.0))
+            {
+                Debug.Log("Ataque");
+                status = 1;
+            }
+            else
+            {
+                nav.SetDestination(destPos);
+            }
+
+        }
+
+        void UpdateAttackState()
+        {
+            /*Debug.Log("Ataque");
+            Debug.Log(player.position.x);
+            Debug.Log(player.position.y);
+            Debug.Log(inimigos.position.x);
+            Debug.Log(inimigos.position.y);*/
+            nav.SetDestination(player.position);
+            if ((Mathf.Abs(player.position.x - inimigos.position.x) >= 5.0) && (Mathf.Abs(player.position.y - inimigos.position.y) >= 5.0))
+            {
+                nav.SetDestination(player.position);
+            }
+            else
+            {
+                Debug.Log("Patrol");
+                status = 0;
+            }
+
+        }
     }
+
 }
